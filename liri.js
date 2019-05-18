401d9390a8e27ba639a9f45fe4e2efe05a5ae82b
Erica Walsh <erica.f.walsh@gmail.com>
2019-05-18 13:13:24 -0400

    updates

--- liri.js
+++ liri.js
@@ -1,31 +1,42 @@
-
-
+// Requiring axios, dotenv, spotify api, and fs, etc.
 require("dotenv").config();
 var keys = require("./keys.js");
-
-
-// require axios
 var axios = require("axios");
-
-// require spotify
 var Spotify = require('node-spotify-api');
-
 var spotify = new Spotify(keys.spotify);
-
 var fs = require("fs");
 
+// Defining variables
 var songName = "";
 var movieName = "";
 
-// Defining a function for a spotify function
-// Need to make sure default song is "The Sign" by Ace of Base. and make it work with spaces
+// Defining a function for a spotify api call since it is used twice
+// Need to make sure default song is "The Sign" by Ace of Base. 
 function spotifyCall() {
 
     if (process.argv[3] === undefined) {
+
         songName = "The Sign";
+
+        spotifySong = response.tracks.items;
+
+            // Loop through songs to make sure only Ace of Base version is printed
+            // NEED TO MAKE SURE THIS WORKS
+            for (i=0;i<spotifySong.length;i++) {
+
+                if (spotifySong[i].album.artists[0].name === "Ace of Base") {
+                    console.log("RESULT " + parseInt(i+1));
+                    console.log("Artist(s): " + artists);
+                    console.log("The song's name is: " + songsName);
+                    console.log("Link to the song on Spotify: " + previewLink);
+                    console.log("The album's name: " + albumName);
+                }
+            };
+
+
     }
     else {
-        songName = process.argv[3];
+        songName = process.argv.slice(3).join(" ");
     };
 
     spotify.search({ type: 'track', query: songName }, function (err, response) {
@@ -33,61 +44,63 @@ function spotifyCall() {
             return console.log(err);
         }
 
+        spotifySong = response.tracks.items;
+        
+        for (i=0; i<spotifySong.length; i++) {
 
-        var artists = response.tracks.items[0].album.artists[0].name;
-        var songsName = response.tracks.items[0].name;
-        var albumName = response.tracks.items[0].album.name;
-        var previewLink = response.tracks.items[0].external_urls.spotify;
-
-        console.log("Artist(s): " + artists);
-        console.log("The song's name is: " + songsName);
-        console.log("Link to the song on Spotify: " + previewLink);
-        console.log("The album's name: " + albumName);
+            var artists = spotifySong[i].album.artists[0].name;
+            var songsName = spotifySong[i].name;
+            var previewLink = spotifySong[i].external_urls.spotify;
+            var albumName = spotifySong[i].album.name;
+    
+            console.log("RESULT " + parseInt(i+1));
+            console.log("Artist(s): " + artists);
+            console.log("The song's name is: " + songsName);
+            console.log("Link to the song on Spotify: " + previewLink);
+            console.log("The album's name: " + albumName);
+        } 
 
     });
 };
 
-// make it OK with less than 10 events
+// If the user enters the "concert-this" command, then:
 if (process.argv[2] === "concert-this") {
 
-    var artist = process.argv[3];
+    var artist = process.argv.slice(3).join(" ");
 
     axios
         .get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp")
 
         .then(function (response) {
 
-            // If there are more than 10 upcoming events, only show the next 10
-            for (i = 0; i < 10; i++) {
-                var venueName = response.data[i].venue.name;
-                var venueLocation = response.data[i].venue.city + ", " + response.data[i].venue.country;
-                var date = response.data[i].datetime;
+            var concertData = response.data;
 
-                console.log("Upcoming Event " + (i + 1));
+            for (i = 0; i < concertData.length; i++) {
+                var venueName = concertData[i].venue.name;
+                var venueLocation = concertData[i].venue.city + ", " + response.data[i].venue.country;
+                var date = concertData[i].datetime;
+
+                console.log("UPCOMING EVENT " + parseInt(i+1));
                 console.log("Venue Name: " + venueName);
                 console.log("Venue Location: " + venueLocation);
                 console.log("Date: " + date.substring(0, 10));
             }
         });
-
 }
 
-
+// If the user enters "spotify-this-song" command, then call the spotifyCall function
 else if (process.argv[2] === "spotify-this-song") {
-
     spotifyCall();
-
 }
 
-// Need to make it work with spaces
-// Need to fix rotten tomatoes rating
+// If the user enters the "movie-this" command, then:
 else if (process.argv[2] === "movie-this") {
 
     if (process.argv[3] === undefined) {
         movieName = "Mr. Nobody";
     }
     else {
-        movieName = process.argv[3];
+        movieName = process.argv.slice(3).join(" ");
     };
 
     axios
@@ -95,19 +108,29 @@ else if (process.argv[2] === "movie-this") {
 
         .then(function (response) {
 
-            console.log("Movie title: " + response.data.Title);
-            console.log("Year it came out: " + response.data.Year);
-            console.log("IMDB Rating: " + response.data.imdbRating);
-            // Need to fix rating
-            console.log("Rotten Tomatoes Rating: " + response.data.Ratings[1].Value);
-            console.log("Language: " + response.data.Language);
-            console.log("Plot: " + response.data.Plot);
-            console.log("Actors: " + response.data.Actors);
+            var movieData = response.data;
+
+            console.log("Movie title: " + movieData.Title);
+            console.log("Year it came out: " + movieData.Year);
+            console.log("IMDB Rating: " + movieData.imdbRating);
+
+            // Loop through ratings to make sure only Rotten Tomatoes rating is printed
+            for (i=0;i<movieData.Ratings.length;i++) {
+
+                if (movieData.Ratings[i].Source === "Rotten Tomatoes") {
+                    console.log("Rotten Tomatoes Rating: " + movieData.Ratings[i].Value);
+                }
+            };
+
+            console.log("Language: " + movieData.Language);
+            console.log("Plot: " + movieData.Plot);
+            console.log("Actors: " + movieData.Actors);
 
         });
 
 }
 
+// If the user enters the "do-what-it-says" command, then:
 else if (process.argv[2] === "do-what-it-says") {
 
     fs.readFile("random.txt", "utf8", function (error, data) {
